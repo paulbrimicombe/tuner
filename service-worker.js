07c69bc9b0a87dd0ec579095d359a33d53898223
Paul Douglas Brimicombe <paul.brimicombe@bbc.co.uk>
2024-02-02 14:28:51 +0000

    More performance testing

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_STORAGE_KEY = "tuner-v1.0.43";
+const CACHE_STORAGE_KEY = "tuner-v1.0.44";
 
 const cachedPaths = [
   "./",
@@ -14,7 +14,7 @@ const cachedPaths = [
   "./styles/main.css",
   "./styles/reset.css",
   "./styles/tuner.css",
-];  
+];
 
 self.oninstall = (event) => {
   event.waitUntil(
